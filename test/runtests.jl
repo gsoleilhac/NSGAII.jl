a5c67fe6c87b2ec84eb2d87d01af4021ca9008e9
Gauthier Soleilhac <gsoleilhac@gmail.com>
2018-02-19 17:29:20 +0100

    adds tests for vOpt

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using NSGAII
+using NSGAII, vOptGeneric, vOptSpecific
 using Compat.Test, Compat.Random
 
 
@@ -22,3 +22,27 @@ res = nsga(500, 200, z, d, seed = seed)
 @test minimum(x -> x.y[1], res) <= 1e-4
 @test maximum(x -> x.y[2], res) >= 3.99
 @test minimum(x -> x.y[2], res) <= 1e-4
+
+
+m = vModel()
+id = load2UKP(joinpath(@__DIR__, "2KP500-1A.DAT"))
+p1, p2, w, c = id.P1, id.P2, id.W, id.C
+@variable(m, x[1:length(p1)], Bin)
+@addobjective(m, Max, dot(x, p1))
+@addobjective(m, Max, dot(x, p2))
+@constraint(m, dot(x, w) <= c)
+@inferred nsga(Val(2), 100, 200, m)
+
+m = vModel()
+@variable(m, 0 <=x[1:5] <= 10)
+@variable(m, 0 <= δ[1:3] <= 1, Int)
+@addobjective(m, Max, dot([17,-12,-12,-19,-6], x) + dot([-73, -99, -81], δ))
+@addobjective(m, Max, dot([2,-6,0,-12,13], x) + dot([-61,-79,-53], δ))
+@addobjective(m, Max, dot([-20,7,-16,0,-1], x) + dot([-72,-54,-79], δ))
+@constraint(m, sum(δ) <= 1)
+@constraint(m, -x[2] + 6x[5] + 25δ[1] <= 52)
+@constraint(m, -x[1] + 18x[4] + 18x[5] + 8δ[2] <= 77)
+@constraint(m, 7x[4] + 9x[5] + 19δ[3] <= 66)
+@constraint(m, 16x[1] + 20x[5] <= 86)
+@constraint(m, 13x[2] + 7x[4] <= 86)
+@inferred nsga(Val(3), 100, 200, m)
