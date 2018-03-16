1c079bf0ff9cd364890b207cde1880e2d28bb726
Gauthier Soleilhac <gsoleilhac@gmail.com>
2018-03-16 10:29:41 +0100

    Update NSGAII.jl

--- src/NSGAII.jl
+++ src/NSGAII.jl
@@ -2,7 +2,7 @@ __precompile__()
 module NSGAII
 
 export nsga, nsga_max, nsga_binary, BinaryCoding
-using ProgressMeter, Compat, Compat.Random
+using ProgressMeter, Compat, Compat.Random, Compat.LinearAlgebra
 
 include("indivs.jl")
 include("functions.jl")
