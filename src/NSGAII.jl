0bcb40d4d8b65aabcc611667ada0918352318d8a
Gauthier Soleilhac <gsoleilhac@gmail.com>
2018-03-19 17:42:24 +0100

    removes init function from args when passing a BinaryCoding

--- src/NSGAII.jl
+++ src/NSGAII.jl
@@ -18,25 +18,25 @@ function nsga(popSize::Integer, nbGen::Integer, z::Function, init::Function ;
     return _nsga(X, Min(), popSize, nbGen, init, z, fdecode, fdecode!, fCV , pmut, fmut, fcross, seed, fplot, plotevery, showprogress ? 0.5 : Inf)
 end
 
-function nsga(popSize::Integer, nbGen::Integer, z::Function, bc::BinaryCoding, init::Function=()->bitrand(bc.nbbitstotal); 
+function nsga(popSize::Integer, nbGen::Integer, z::Function, bc::BinaryCoding ; 
     fCV = x->0., pmut= 0.05, fmut=default_mutation!, fcross = default_crossover!, 
     seed=Vector{Float64}[], fplot = (x)->nothing, plotevery=1, showprogress = true)
     X = create_indiv(init(), x->decode(x, bc), z, fCV)
-    return _nsga(X, Min(), popSize, nbGen, init, z, x->decode(x, bc), (g,f)->decode!(g, bc, f), fCV , pmut, fmut, fcross, encode.(seed, bc), fplot, plotevery, showprogress ? 0.5 : Inf)
+    return _nsga(X, Min(), popSize, nbGen, ()->bitrand(bc.nbbitstotal), z, x->decode(x, bc), (g,f)->decode!(g, bc, f), fCV , pmut, fmut, fcross, encode.(seed, bc), fplot, plotevery, showprogress ? 0.5 : Inf)
 end
 
 function nsga_max(popSize::Integer, nbGen::Integer, z::Function, init::Function ; 
     fdecode=identity, fdecode! = (geno,pheno)-> pheno.=geno, fCV = x->0., pmut= 0.05, fmut=default_mutation!, 
     fcross = default_crossover!, seed=typeof(init())[], fplot = (x)->nothing, plotevery=1, showprogress = true)
 	X = create_indiv(init(), fdecode, z, fCV)
-    return _nsga(X, Max(), popSize, nbGen, init, z, fdecode, fdecode!, fCV , pmut, fmut, fcross, seed, fplot, plotevery, showprogress ? 0.5 : Inf)
+    return _nsga(X, Max(), popSize, nbGen, ()->bitrand(bc.nbbitstotal), z, fdecode, fdecode!, fCV , pmut, fmut, fcross, seed, fplot, plotevery, showprogress ? 0.5 : Inf)
 end
 
-function nsga_max(popSize::Integer, nbGen::Integer, z::Function, bc::BinaryCoding, init::Function=()->bitrand(bc.nbbitstotal); 
+function nsga_max(popSize::Integer, nbGen::Integer, z::Function, bc::BinaryCoding ;
     fCV = x->0., pmut= 0.05, fmut=default_mutation!, fcross = default_crossover!, 
     seed=Vector{Float64}[], fplot = (x)->nothing, plotevery=1, showprogress = true)
-	X = create_indiv(init(), x->decode(x, bc), z, fCV)
-    return _nsga(X, Max(), popSize, nbGen, init, z, x->decode(x, bc), (g,f)->decode!(g, bc, f), fCV , pmut, fmut, fcross, encode.(seed, bc), fplot, plotevery, showprogress ? 0.5 : Inf)
+    X = create_indiv(init(), x->decode(x, bc), z, fCV)
+    return _nsga(X, Max(), popSize, nbGen, ()->bitrand(bc.nbbitstotal), z, x->decode(x, bc), (g,f)->decode!(g, bc, f), fCV , pmut, fmut, fcross, encode.(seed, bc), fplot, plotevery, showprogress ? 0.5 : Inf)
 end
 
 end # module
