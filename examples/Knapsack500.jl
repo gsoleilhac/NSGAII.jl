dfc663c9f446f66acb97918ebdb4791f8551c68c
Gauthier Soleilhac <gsoleilhac@gmail.com>
2018-03-23 17:06:39 +0100

    update knapsack example

--- examples/Knapsack500.jl
+++ examples/Knapsack500.jl
@@ -9,28 +9,27 @@ p1, p2, w, c = id.P1, id.P2, id.W, id.C
 @addobjective(m, Max, dot(x, p2))
 @constraint(m, dot(x, w) <= c)
 
-function plot_pop(P)
+function plot_pop(P, titre)
     clf()
     ax = gca()
     ax[:set_xlim]([15800, 20570])
     ax[:set_ylim]([15630, 20877])
-    p = plot(map(x -> x.y[1], P), map(x -> x.y[2], P), "bo", markersize=1)
+    p = plot(map(x -> x.y[1], P), map(x -> x.y[2], P), "bo", markersize=1, label="nsga")
+    title(titre)
     !isinteractive() && show()
     sleep(0.1)
 end
 
-nsga(250, 100, m, fplot=plot_pop)
+nsga(100, 5000, m, fplot = p->plot_pop(p, "without seed"), plotevery=500)
 
 solve(m, method=:dichotomy)
 
 Y_N = getY_N(m)
-
 seed = [getvalue(x, 1), getvalue(x, length(Y_N)), getvalue(x, length(Y_N)÷2)]
-nsga(250, 100, m, fplot=plot_pop, seed=seed)
+nsga(100, 5000, m, fplot = p->plot_pop(p, "with seed"), seed=seed, plotevery=500)
 
 f1 = map(y -> y[1], Y_N)
 f2 = map(y -> y[2], Y_N)
-xlabel("z1")
-ylabel("z2")
-plot(f1,f2,"bx", markersize = "6")
-!isinteractive() && show()
+xlabel("z1") ; ylabel("z2")
+p = plot(f1,f2,"kx", markersize = "2", label="exact")
+legend() ; display(p)
